62136dbb86fbfd20f6ac2a14b930dc5d0d2de124
Brian Timba <bryantymbr@gmail.com>
2026-09-03 19:33:26 +0200

    Remove MongoDB connection module and related functions

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-// import { getDb } from './src/db/connect.js';
 
 const app = express();
 
@@ -8,15 +7,5 @@ app.use(express.json());
 app.get('/', (req, res) => {
     return res.status(200).json ({ message: 'Server is running' });
 });
-
-// /* // app.get('/trails', async (req, res) => {
-// //     try {
-// //         const trails = await getDb().collection('trails').find().toArray();
-// //         return res.status(200).json(trails);
-// //     } catch (error) {
-// //         console.error('Error fetching trails:', error);
-// //         return res.status(500).json({ error: 'Internal Server Error' });
-// //     }
-// // }); */
 
 export default app;
