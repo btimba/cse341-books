c82049bc7f2ee634e8cf6b18d5681a5f2e008ad4
Brian Timba <bryantymbr@gmail.com>
2026-09-03 12:57:15 +0200

    Connected app to MongoDb

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+// import { getDb } from './src/db/connect.js';
 
 const app = express();
 
@@ -7,5 +8,15 @@ app.use(express.json());
 app.get('/', (req, res) => {
     return res.status(200).json ({ message: 'Server is running' });
 });
+
+// /* // app.get('/trails', async (req, res) => {
+// //     try {
+// //         const trails = await getDb().collection('trails').find().toArray();
+// //         return res.status(200).json(trails);
+// //     } catch (error) {
+// //         console.error('Error fetching trails:', error);
+// //         return res.status(500).json({ error: 'Internal Server Error' });
+// //     }
+// // }); */
 
 export default app;
